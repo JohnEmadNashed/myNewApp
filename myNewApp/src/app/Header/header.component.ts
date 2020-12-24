5385475ff11aa732613674b91915188e6cc076d9
John Nashed <john15696@gmail.com>
2020-12-24 13:46:30 +0200

    First Commit

---
+++ myNewApp/src/app/Header/header.component.ts
@@ -0,0 +1,16 @@
+//This is the Application Header only showning My messages title
+
+
+import { componentFactoryName, templateJitUrl } from '@angular/compiler';
+import {Component} from "@angular/core";
+@Component(
+  {
+    selector:'app-header',
+    templateUrl: './header.component.html'
+  }
+)
+
+export class headerComponent
+{
+
+}
---
+++ myNewApp/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule  } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './Posts/post-create/post-create.component'
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { headerComponent } from './Header/header.component';
+import { PostListComponent } from './Posts/post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    headerComponent,
+    PostListComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
