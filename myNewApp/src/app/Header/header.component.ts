b86b4f6ffe7c8c8966da62273e026973595db14c
John Nashed <john15696@gmail.com>
2020-12-24 15:39:23 +0200

    Module4.5

--- myNewApp/src/app/Header/header.component.ts
+++ myNewApp/src/app/Header/header.component.ts
@@ -6,7 +6,8 @@ import {Component} from "@angular/core";
 @Component(
   {
     selector:'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
   }
 )
 
